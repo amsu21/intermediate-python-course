6e035a6ed80c03d6415276b7f8ccbdce5a2c0e01
Amsu Warner <aswb4@hotmail.com>
2021-07-09 22:46:43 -0400

    Imported module and made a variable

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,10 @@
+import random
+
+
 def main():
-    print('You rolled a die')
+    # INTITALIZE VARS
+    roll = random.randint(1, 6)     # GIVES US A RANDOM INT BETWEEN 1 - 6
+    print(f'You rolled a {roll}')
 
 
 if __name__ == "__main__":
